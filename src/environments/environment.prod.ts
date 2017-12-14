02eca51196bcf2f4d879a6e965efad7bc5e70c4b
Alexander Midtsev <midtsev@gmail.com>
2017-12-14 22:50:58 +0200

    feature(environment)
    * Set urls;

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  socket_path: 'http://localhost:3000',
+  socket_path: 'http://172.104.130.239:3000',
+  api_path: 'http://172.104.130.239:4000',
   images_path: 'https://image.eveonline.com',
 };
